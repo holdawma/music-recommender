35e55450136f4279fdd1ecca46be5ea6fe009cac
Matthew Holdaway <matthew.holdaway@student.onslow.school.nz>
2020-07-06 13:24:44 +1200

    Initial sharing of project

---
+++ Album.java
@@ -0,0 +1,33 @@
+/* Based on the ecs 100 template
+ * Code for all albums in in the program
+ * Name:
+ * Date:
+ */
+
+
+import ecs100.*;
+import java.util.*;
+import java.io.*;
+import java.awt.Color;
+
+
+/** 
+ * Will be able to add an album and then store that album in a hashmap
+ */
+public class Album {
+    // Set variables
+    private String name;
+    private String genre;
+    private String artist;
+    
+    /**
+     * Constructor for Album class
+     */
+    public Album() {
+        
+    }
+
+    
+
+}
+
