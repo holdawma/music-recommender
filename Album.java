52143010727eb4960f2ef2531617471f129b59cf
Matthew Holdaway <matthew.holdaway@student.onslow.school.nz>
2020-07-27 12:28:03 +1200

    Changed to 2 class format All of sprint 1 complete 	- Add and store new album 	- Add and store rating 	- Functioning GUI

--- Album.java
+++ Album.java
@@ -12,22 +12,40 @@ import java.awt.Color;
 
 
 /** 
- * Will be able to add an album and then store that album in a hashmap
+ * Class for each album, puts all values into variables, can return each value
+ * String name
+ * String genre
+ * String artist
+ * Boolean liked
  */
 public class Album {
     // Set variables
     private String name;
     private String genre;
     private String artist;
+    private boolean liked;
     
-    /**
-     * Constructor for Album class
-     */
-    public Album() {
-        
+    public Album(String userName, String userGenre, String userArtist, boolean userLikeAlbum) {
+        this.name = userName;
+        this.genre = userGenre;
+        this.artist = userArtist;
+        this.liked = userLikeAlbum;
     }
-
     
-
+    public String getName() {
+        return this.name;
+    }
+    
+    public String getGenre() {
+        return this.genre;
+    }
+    
+    public String getArtist() {
+        return this.artist;
+    }
+    
+    public boolean getRating() {
+        return this.liked;
+    }
 }
 
--- Albums.java
+++ Albums.java
@@ -1,33 +1,102 @@
-
-/**
- * Write a description of class Albums here.
- *
- * @author (your name)
- * @version (a version number or a date)
+/* Based on the ecs 100 template
+ * Code for ??
+ * Name:
+ * Date:
  */
-public class Albums
-{
-    // instance variables - replace the example below with your own
-    private int x;
+
+import ecs100.*;
+import java.util.*;
+import java.io.*;
+import java.awt.Color;
+
+/** The class for all albums
+ * String name
+ * String genre
+ * String artist
+ * boolean liked
+ */
+public class Albums {
+
+    private String name;
+    private String genre;
+    private String artist;
+    private boolean liked = false;
+    private HashMap<String, Album> albumStore; 
 
     /**
-     * Constructor for objects of class Albums
+     * Constructor for class Albums
+     * Builds hashmap and calls main menu method
      */
-    public Albums()
-    {
-        // initialise instance variables
-        x = 0;
+    public Albums() {
+        albumStore = new HashMap<String, Album>();
+        mainMenu();
+    }
+    
+    /**
+     * Shows main menu options
+     * 
+     */
+    public void mainMenu() {
+        UI.initialise();
+        UI.addButton("Add new Album", this::addAlbumUI);
+        UI.addButton("View All", this::viewAll);
+    }
+    
+    /**
+     * Shows buttons and text fields for adding an album
+     */
+    public void addAlbumUI(){
+        UI.initialise();
+        UI.addTextField("Album Name: ", this::addName);
+        UI.addTextField("Album Genre: ", this::addGenre);
+        UI.addTextField("Album Artist: ", this::addArtist);
+        UI.addButton("Like", this::likeAlbum);
+        UI.addButton("Add Album", this::addAlbum);
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    private void addName(String userName){
+        name = userName;
     }
+
+    private void addGenre(String userGenre){
+        genre = userGenre;
+    }
+
+    private void addArtist(String userArtist){
+        artist = userArtist;
+    }
+
+    private void likeAlbum(){
+        if (liked == true) {
+            liked = false;
+            UI.println("Disliked");
+        } else if (liked == false) {
+            liked = true;
+            UI.println("Liked");
+        }
+    }
+
+    public void addAlbum() {
+        Album newAlbum = new Album(name, genre, artist, liked);
+        albumStore.put(name, new Album(name, genre, artist, liked));
+        mainMenu();
+    }
+
+    public void viewAll() {
+        for (String i : albumStore.keySet()) {
+            String gen = albumStore.get(i).getGenre();
+            String art = albumStore.get(i).getArtist();
+            boolean rat = albumStore.get(i).getRating();
+            UI.println("Name: " + i);
+            UI.println("Genre: " + gen);
+            UI.println("Artist: " + art);
+            UI.println("Rating: " + rat);
+        }
+    }
+
+    public static void main(String[] args){
+        Albums obj = new Albums();
+    }
+
 }
+
